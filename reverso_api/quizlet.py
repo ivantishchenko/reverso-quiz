b486f6e821974c234b96821d02ffae207d191467
Ivan Tishchenko <johnny.tishchenko@gmail.com>
2018-04-01 21:59:51 +0200

    Eliminated dublicates in a dictionary. Addded flag for pushing to quizlets

--- reverso_api/quizlet.py
+++ reverso_api/quizlet.py
@@ -1,7 +1,9 @@
 import requests
+import csv
 
 class QuizletAPI:
 
+    OUT_NAME = 'quiz.csv'
     SET_ID = '282592606'
     CREDENTIALS_FILE = 'credentials.txt'
     URL = 'https://api.quizlet.com/2.0/sets/' + SET_ID + '/terms'
@@ -20,7 +22,26 @@ class QuizletAPI:
         data = {'term':term, 'definition':definition}
         r = requests.post(self.URL, headers=headers, data=data)
         res = r.text
+        print("Created card...")
         print(res)
 
+    def postMissingCards(self):
+        print("Posting missing cards")
+        cache = []
+        with open(self.OUT_NAME, 'r', newline="") as csvfile:
+            reader = csv.reader(csvfile, delimiter=",")
+            for i, line in enumerate(reader):
+                if line[4] == '0':
+                    self.postCard(line[1], line[2])
+                    line[4] = '1'
+                cache.append(line)
+
+        with open(self.OUT_NAME, 'w', newline="") as csvfile:
+            dict_writer = csv.writer(csvfile, delimiter=',')
+            for line in cache:
+                dict_writer.writerow(line)
+                
+
+
 a = QuizletAPI()
-a.postCard("hello", "wolrd")
+a.postMissingCards()
--- reverso_api/reverso.py
+++ reverso_api/reverso.py
@@ -9,8 +9,22 @@ class DictionaryAPI:
 
     def __init__(self):
         print("Create the API object...")
+        self.readCSVDictionary()
         #print(self.URL)
     
+    def readCSVDictionary(self):
+        try:
+            with open(self.OUT_NAME) as f:
+                # self.dict_content = [line.split() for line in f]
+                # print(self.dict_content)
+                self.dict_content = []
+                reader = csv.reader(f, delimiter=",")
+                for i, line in enumerate(reader):
+                    self.dict_content.append(line)
+        except IOError:
+            print("Dictionary CSV doen't exist...")
+            self.dict_content = []
+
     def getData(self):
         print("Getting the JSON data for a user " + self.USER + "...")
         headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36'}
@@ -18,11 +32,18 @@ class DictionaryAPI:
         res = r.json()
         return res
     
+    def recordExists(self, src, trg):
+        for i, line in enumerate(self.dict_content):
+            if line[0] == src and line[2] == trg:
+                return True
+            # print("line{0} = {1}".format(i, line))
+        return False
+
     def buildCSV(self):
         res = self.getData()
         dictonaryData = res['data']
 
-        with open(self.OUT_NAME, 'w', newline="") as csvfile:
+        with open(self.OUT_NAME, 'a', newline="") as csvfile:
             dict_writer = csv.writer(csvfile, delimiter=',')
             for each_entry in dictonaryData:
                 src = each_entry['srcText']
@@ -30,15 +51,11 @@ class DictionaryAPI:
                 context = each_entry['srcContext']
                 date = each_entry['creationDate']
 
-                context = context.replace('<em>', '*').replace('</em>', '*')
-                src = "*" + src + "*"
-
-                term = src + " - [" + context + "]"
-
-                dict_writer.writerow([term, trg, date])
-                #dict_writer.writerow([src + " - [" + context +"]", trg])
-
-
+                if self.recordExists(src, trg) == False:
+                    context = context.replace('<em>', '*').replace('</em>', '*')
+                    term = "*" + src + "*"
+                    term = term + " - [" + context + "]"
+                    dict_writer.writerow([src, term, trg, date])
 
 
 a = DictionaryAPI()
